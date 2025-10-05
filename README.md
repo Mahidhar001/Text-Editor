1c6c65918e6920cfff24d014b5a25c48bb371f62
mahidhartatipakala <mahidhartatipakala@gmail.com>
2025-10-05 14:14:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Text Editor](https://mahidhar001.github.io)
+
+#### A simple text editor for web.
---
+++ main.js
@@ -0,0 +1,11 @@
+function downloadFile() {
+    var text = document.getElementById('textbox').value;
+    var blob = new Blob([text], {type: 'text/plain'});
+    var link = document.createElement('a');
+    
+    link.href = URL.createObjectURL(blob);
+    link.download = 'text.txt'; 
+    link.click();
+    
+    URL.revokeObjectURL(link.href);
+  }
